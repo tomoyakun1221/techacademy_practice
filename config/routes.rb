2052dac72d119128efc813cfaded27889fb62df0
EC2 Default User <ec2-user@ip-172-31-41-196.ap-northeast-1.compute.internal>
2019-09-12 11:23:43 +0000

    タイトル実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users do
+    resources :attendances
+  end
 end
