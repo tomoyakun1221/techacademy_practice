3f244efb391e3a64f4a40fa4b8a93ea013e47314
EC2 Default User <ec2-user@ip-172-31-41-196.ap-northeast-1.compute.internal>
2019-09-28 15:48:30 +0000

    ログイン_永続的セッション実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -11,4 +12,37 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :password, presence: true, length: { minimum: 6 }
+  
+   # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  #ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64  
+  end
+  
+  #永続セッションのユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  #記憶トークンが記憶ダイジェストと一致すればtrueを返
+  def authenticate?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #ユーザーのログイン状態を破棄
+  def forget
+    update_attribute(:remember_digest, nil) 
+  end
 end
