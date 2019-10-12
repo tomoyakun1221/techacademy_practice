45e314f370e9c9d1fe51ce4f8e3d889a1909ebd8
EC2 Default User <ec2-user@ip-172-31-41-196.ap-northeast-1.compute.internal>
2019-10-12 10:22:42 +0000

    勤怠コントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,91 +1,94 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
-  
+  before_action :set_one_month, only: :show
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
-  
-  def show
-  end
-  
-  def index
-    @users = User.paginate(page: params[:page], per_page: 20)
-  end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "ユーザーの新規作成に成功しました"
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "ユーザーの更新に成功しました"
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
-  
+
   def destroy
     @user.destroy
-    flash[:danger] = "削除されました"
+    flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
-  
+
   def edit_basic_info
   end
-  
+
   def update_basic_info
     if @user.update_attributes(basic_info_params)
-      flash[:success] = "#{@user.name}の更新が成功しました"
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
     else
       flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
     end
     redirect_to users_url
   end
-  
+
   private
-    
+
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-    
+
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
-    
-    #logged_userで@user(インスタンス変数)で定義しており、さらにshow.edit.updateで@userを定義するのはくどいので、set_userで設定する
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
-    
-    #編集.更新.詳細.削除画面閲覧はログインしているユーザーのみ可能にするための判定
+
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "ログインして下さい"
+        flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
     end
-    
-    #編集.更新は、現在ログインしているユーザーのみ可能にするための判定
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to root_url unless current_user?(@user) 
+      redirect_to(root_url) unless current_user?(@user)
     end
-    
+
+    # システム管理権限所有かどうか判定します。
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
