131479f060a05430fcc46cdcc9657d5d34f8ff8a
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-07-30 13:35:00 +0000

    usermodelの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :uid, presence: true, length: { maximum: 20 }
+  validates :role, presence: true, length: { maximum: 1 }
+  validates :password, presence: true
+  has_secure_password
 end
