d78a1a39e9cd08cc311218c12ff7ac2be688bf21
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-07-30 13:26:22 +0000

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210730132156) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "name"
+    t.integer "role"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
