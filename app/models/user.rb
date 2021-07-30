1603f2ce8058c7a451b0078f899729b26dd4cc4c
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-07-30 14:07:00 +0000

    locales modelsの登録語の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :uid, presence: true, length: { maximum: 20 }
+  validates :uid, presence: true
   validates :role, presence: true, length: { maximum: 1 }
   validates :password, presence: true
   has_secure_password
