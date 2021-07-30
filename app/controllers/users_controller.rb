51a68c5e4626f6d0d8a5e92599d9102945eca839
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-07-30 13:50:29 +0000

    static_pages topのlink先の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root to: 'static_pages#top'
 
   get 'signup', to: 'users#new'
