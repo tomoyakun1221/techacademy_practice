6b68c80e62eed90d9e737e50d10e54e7e1359b05
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-07-31 12:14:17 +0000

    ログイン、ログアウト機能用ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#top'
-
-  resources :users,only: [:index, :new, :create, :edit, :show]
+  
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
 end
