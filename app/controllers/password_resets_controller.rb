83eb081a19f1943caa0dcd42c0683cf45079b2b1
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-02 09:15:05 +0000

    ＜パスワード再設定機能実装＞ ・ルーティング追加 ・コントローラー、view追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,43 @@ class PasswordResetsController < ApplicationController
   def new
   end
   
+  def create
+    uid = params[:password_resets][:uid]
+    @user = User.find_by(uid: uid)
+    
+    if @user
+      session[:uid] = uid
+      redirect_to edit_password_reset_path(@user.id)
+    else
+      flash.now[:danger] = '職員コードが存在しません。'
+      render :new
+    end
+  end
   
+  def edit
+    @user = User.find(params[:id])
+    
+    if @user && (@user.uid == session[:uid])
+      session.delete(:uid)
+      return @user
+    else
+      session.delete(:uid)
+      flash.now[:danger] = '職員コードを入力してください。'
+      render :new
+    end
+    
+  end
   
+  def update
+    @user = User.find(params[:id])
+    password_params = params.permit(:password, :password_confirmation)
+    
+    if @user.update(password_params)
+      flash[:success] = 'パスワードを変更しました。'
+      redirect_to login_path
+    else
+      flash.now[:danger] = 'パスワードの変更に失敗しました。'
+      render :edit
+    end
+  end
 end
