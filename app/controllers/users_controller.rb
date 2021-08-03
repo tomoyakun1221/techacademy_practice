e724d3a72b3386a4d0d7b7b730519c97f33d8f82
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-03 23:08:49 +0000

    一部修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :update_index, :destroy]
   
   def index
     @users = User.all
