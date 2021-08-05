b1f27e83193843572b17b2ea1ae10b07ffc3218c
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-05 01:48:26 +0000

    ユーザー一覧_エラー表示できるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,13 +29,23 @@ class UsersController < ApplicationController
       redirect_to root_url
     else
       flash.now[:danger] = 'ユーザー情報の更新に失敗しました。'
+      render 'edit'
+    end
+  end
+  
+  def update_index
+    if @user.update_attributes(user_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
       @users = User.all
       render 'index'
     end
   end
   
   def destroy
-    @user.destroy
+    @user.destroys
     flash[:success] = 'ユーザーを削除しました。'
     redirect_back(fallback_location: root_path)
   end
