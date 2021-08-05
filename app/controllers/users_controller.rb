93ac9486a4fabacb35317fa58a2a42923e590b1c
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-05 13:38:38 +0000

    csvfile読み込み成功時のflashメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,7 @@ class UsersController < ApplicationController
   
   def import
     if params[:file]
+      flash[:success] = 'ファイルを読み込みました'
       User.import(params[:file])
       redirect_to users_url
     else
