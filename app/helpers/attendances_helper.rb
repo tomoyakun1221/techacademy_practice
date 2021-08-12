35845a977f942174bc40106875e505427d61ae0f
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-12 13:02:34 +0000

    fix

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -2,6 +2,6 @@ require 'rounding'
 
 module AttendancesHelper
   def worked_time(start, finish)
-    format("%.2f", (((finish - start).ceil_to(15.minutes) / 60 )/ 60.0) )
+    format("%.2f", (((finish - start) / 60 )/ 60.0) )
   end
 end
