24e18318dba23f0f15fc0a017144fd14a62b642f
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-12 12:14:35 +0000

    在社時間の表示の修正

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,5 +1,7 @@
+require 'rounding'
+
 module AttendancesHelper
   def worked_time(start, finish)
-    format("%.2f", (((finish - start) / 60 ) / 60.0))
+    format("%.2f", (((finish - start).ceil_to(15.minutes) / 60 )/ 60.0) )
   end
 end
