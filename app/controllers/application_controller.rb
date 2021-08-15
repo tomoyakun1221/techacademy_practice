750f5550eaf739edaa74be758835d97eff3dd1e9
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-1.compute.internal>
2021-08-15 01:49:58 +0000

    管理ユーザー以外は管理者画面に遷移しない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,27 +24,37 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
-  $days_of_the_week = %w{日 月 火 水 木 金 土}
-
-  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
-  def set_one_month
-    @first_day = params[:date].nil? ?
-                 Date.current.beginning_of_month : params[:date].to_date
-    @last_day = @first_day.end_of_month
-    one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
-    # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
-    @attendances = User.find(params[:id]).attendances.where(date: @first_day..@last_day).order(:date)
-
-    unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
-      ActiveRecord::Base.transaction do # トランザクションを開始します。
-        # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
-        one_month.each { |day| User.find(params[:id]).attendances.create!(date: day) }
-      end
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
     end
+  end
+ 
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    redirect_to root_url unless current_user?(@user)
+  end
+ 
+  # システム管理権限所有かどうか判定します。
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
 
-  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
-    redirect_to root_url
+  # 管理者権限を持っているユーザーは勤怠の編集ができない。現在ログイ��しているユーザーを許可します。
+  def admin_or_correct_user
+    @user = User.find(params[:id])
+    if current_user.admin?
+      flash[:danger] = "編集・操作権限がありません"
+      redirect_to root_url
+    end
+    
+    unless current_user?(@user)
+      flash[:danger] = "編集・操作権限がありません"
+      redirect_to root_url
+    end
   end
 
   protect_from_forgery with: :exception
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :update_index, :destroy, :show]
   before_action :set_one_month, only: [:show, :show_only]
+  before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy, :overtime_application_info, :update_index]
+  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: [:index, :destroy]
+  before_action :admin_or_correct_user, only: [:show]
   
   def index
     @users = User.all
